fab81cf5499c4a028e7246a8da9b32fab27e5d2e
elegantuniv <korea651@gmail.com>
2015-05-31 17:03:57 +0900

    seed gh-pages

---
+++ components/info-swipeview/data.js
@@ -0,0 +1,18 @@
+var temp_app = document.querySelector('#temp_app');
+temp_app.intros = [
+	{
+		img: "test.png",
+		title: "Title1",
+		content: "This is content area."
+	},
+	{
+		img: "test.png",
+		title: "Title2",
+		content: "This is content area."
+	},
+	{
+		img: "test.png",
+		title: "Title3",
+		content: "This is content area."
+	}
+]
